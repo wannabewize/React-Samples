fb02ba180613e7e32c20a15fc16cb06f68eb675f
wannabewize <wannabewize@gmail.com>
2020-09-06 03:35:24 +0900

    예제 모음

---
+++ draft/ESM_modules/greeting.js
@@ -0,0 +1,14 @@
+export const message = 'ESM Module Example';
+
+export const sayHello = () => {
+    console.log('Hello');
+}
+
+export function sayGoodBye() {
+    console.log('good bye');
+}
+
+export const helloObj = {
+    message: 'Hello',
+    value: 100
+}

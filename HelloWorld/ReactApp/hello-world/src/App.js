e577b1d5d4fb3f58f373c804b4c66ec0049dabbb
wannabewize <wannabewize@gmail.com>
2020-09-07 06:10:19 +0900

    예제 업데이트

---
+++ HelloWorld/ReactApp/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;

7db9ef6a2e2fadc10c9df7a66e03d3cba147d67a
wannabewize <wannabewize@gmail.com>
2020-09-07 01:24:51 +0900

    예제 업데이트

--- draft/BabelAndWebpack/Step1/index.js
+++ draft/BabelAndWebpack/Step1/index.js
@@ -2,11 +2,12 @@
 // https://reactjs.org/docs/react-api.html#createelement
 // React.createElement(type, [props], [...children])
 
-const e = React.createElement;
 function App() {
-    return e('h1', null, 'Hello React Web!');
+    return React.createElement('h1', null, 'Hello React Web!');
 }
 
 // JSX 포맷 대신 createElement 사용
 // ReactDOM.render(<App />, document.getElementById('root') ); 
-ReactDOM.render(e(App), document.getElementById('root') );
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('root') );

44525ea8f0e96141198e799b5a595ae91fde26c6
wannabewize <wannabewize@gmail.com>
2020-09-14 00:40:27 +0900

    컴포넌트 예제

---
+++ draft/Component/component1/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hot Netflix Movies</h1>
+      <ul>
+        <li>킹덤</li>
+        <li>종이의 집</li>
+        <li>인간수업</li>
+        <li>동백꽃 필 무렵</li>
+      </ul>
+    </div>
+
+  );
+}
+
+export default App;

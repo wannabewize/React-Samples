b2113902e691d8c1ad7180a073f659141fa5e583
wannabewize <wannabewize@gmail.com>
2020-09-28 05:09:46 +0900

    생명주기 예제 업데이트

--- Property,State,Event/component-lifecycle/src/FunctionComponent.js
+++ Property,State,Event/component-lifecycle/src/FunctionComponent.js
@@ -1,20 +1,29 @@
 import React, {useState, useEffect} from 'react';
 
-function ChildFunctionComponent({value}) {
+function ChildFunctionComponent({value1, value2}) {
     useEffect(() => {
+        // value1이 변경될 때만 동작
+        console.log('Child Function Component - value1 changed');
+    }, [value1]);
+
+    useEffect(() => {
+        // mount, unmount, property, state 변경시마다 동작
         console.log('Child Function Component useEffect works');
     });
 
     return (
         <>
         <h3>Child Function Component</h3>
-        <p>props.value : {value}</p>
+        <p>count: {count}</p>
+        <p>props.value1 : {value1}</p>
+        <p>props.value2 : {value2}</p>
         </>
     );
 }
 
 function FunctionComponent() {
-    let [value, setValue] = useState(0);
+    let [value1, setValue1] = useState(0);
+    let [value2, setValue2] = useState(0);
     let [mount, setMount] = useState(true);
 
     useEffect(() => {
@@ -24,13 +33,14 @@ function FunctionComponent() {
     return (
         <div>
         <h1>Function Component</h1>
-        <p>state.value: {value}</p>
-        <button onClick={() => setValue(value+1) }>Increse</button>
+        <p>state.value1: {value1}, state.value2: {value2}</p>
+        <button onClick={() => setValue1(value1+1) }>Increse1</button>
+        <button onClick={() => setValue2(value2+1) }>Increse2</button>
         <button onClick={() => setMount(true) }>Mount</button>
         <button onClick={() => setMount(false) }>Unmount</button>
 
         { mount ?
-            <ChildFunctionComponent value={value}/>
+            <ChildFunctionComponent value1={value1} value2={value2}/>
             : null
         }
         </div>

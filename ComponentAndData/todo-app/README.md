454e3ffa324d1c4b1be1a2beb5de6f9bfb19e3c8
wannabewize <wannabewize@gmail.com>
2020-09-28 00:25:44 +0900

    할일 과제 추가

---
+++ ComponentAndData/todo-app/README.md
@@ -0,0 +1,9 @@
+# TodoExercise
+
+Todo 관리하는 사이트 TodoApp이 일부 구현되어 있습니다.
+
+이를 참고로(원하는 대로 수정 가능) TodoApp을 작성합니다.
+
+- 전체 Todo 개수과 완료한 Todo 개수 출력
+- Todo 추가하기
+- Todo에 체크박스를 이용해서 완료 여부 설정
--- ComponentAndData/todo-app/src/App.js
+++ ComponentAndData/todo-app/src/App.js
@@ -23,29 +23,28 @@ function NewTodoForm({addTodo}) {
     let [todo, setTodo] = useState('');
     return (
         <div>
-            <input value={todo} onChange={e => setTodo(e.target.value) }/>
-            <button onClick={() => addTodo(todo)}>추가</button>
+            <input placeholder='할일을 입력하세요.'/>
+            <button onClick={addTodo}>추가</button>
         </div>
     )
 }
 
 function App() {    
-    let todos = [
-        {id: 1, text: 'study react.js', done: false},
-        {id: 2, text: 'study ReactNative', done: false},
-        {id: 3, text: 'study node.js', done: true}
-    ]
+    let [todos, setTodos] = useState([
+            {id: 1, text: 'study react.js', done: false},
+            {id: 2, text: 'study ReactNative', done: false},
+            {id: 3, text: 'study node.js', done: true}
+    ]);
 
-    function addTodo(todo) {
-        todos.push({id: 0, text: todo, done: false})
-        console.log('addTodo wors :', addTodo);
+    function handleTodoAdd(todo) {
+        console.log('addTodo works :', todo);
     }
 
     return (
         <div>
             <TodoHeader todos={todos} />
             <TodoList todos={todos} />
-            <NewTodoForm addTodo={addTodo}/>
+            <NewTodoForm addTodo={handleTodoAdd}/>
         </div>
     );
 }

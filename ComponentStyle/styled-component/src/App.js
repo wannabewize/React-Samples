d6efe8a0ee71deb60d0d1f7e315a62f5189085bb
wannabewize <wannabewize@gmail.com>
2021-10-12 19:15:20 +0900

    styled component example

--- ComponentStyle/styled-component/src/App.js
+++ ComponentStyle/styled-component/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import './App.css'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const headStyle = {
+    fontSize: '1.5em',
+    textAlign: 'center',
+    color: 'palevioletred'
+};
+
+const Circle = styled.div`
+    width: 5rem;
+    height: 5rem;
+    background: black;
+    border-radius: 50%;
+`;
+
+const ParameterizedCircle = styled.div`
+    border-radius: 50%;
+    ${props =>
+        `
+            background: ${props.color || 'black'};
+            width: ${props.size || 5};
+            height: ${props.size || 5};
+        ` 
+    };        
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>            
+            <h1 className="headStyle">CSS Style By className</h1>
+            <h1 style={headStyle}>CSS Style By Style Object</h1>
+            <Title>Styled Component Example</Title>
+            <Circle />
+            <ParameterizedCircle color="blue" size="50px"/>
+            <ParameterizedCircle color="red " size="10rem"/>
+        </div>
+    );
 }
 
 export default App;
